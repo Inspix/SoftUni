dfa75a745dc7d0cb11d192d42c93deb43be2bdd4
Inspix <Inspix89@gmail.com>
2015-05-12 14:23:55 +0300

    Edit

--- AdvancedCSharp/Homeworks/04.AdvCsharp-RegularExpressions/04.SentenceExtractor/SentenceExtractor.cs
+++ AdvancedCSharp/Homeworks/04.AdvCsharp-RegularExpressions/04.SentenceExtractor/SentenceExtractor.cs
@@ -8,7 +8,7 @@ class SentenceExtractor
         string word = Console.ReadLine();
         string toCheck = Console.ReadLine();
 
-        Regex rgx = new Regex(@"(\w[^.!?]*\b" + word + @"\b[^.!?]*[.!?])");
+        Regex rgx = new Regex(@"(\w[^.!?]*)?(?i:\b" + word + @"\b)[^.!?]*[.!?]");
         MatchCollection matches = rgx.Matches(toCheck);
         foreach (var item in matches)
         {
